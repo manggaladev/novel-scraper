d41de7e7e669f964ed59a526124322c177f82ec5
manggaladev <manggaladev@users.noreply.github.com>
2026-03-23 16:45:23 +0000

    fix: export NovelSpider in __init__.py

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -5,5 +5,9 @@ Novel Scraper Package
 A web scraper for collecting novel data from public websites.
 """
 
+from .spiders.novel_spider import NovelSpider
+
 __version__ = "1.0.0"
 __author__ = "manggaladev"
+
+__all__ = ["NovelSpider"]
